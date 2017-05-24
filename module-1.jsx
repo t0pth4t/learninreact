d2873a42ad15eda573ac7fba70b54abc38113f3f
t0pth4t <t0pth4t@gmail.com>
2017-05-23 21:03:44 -0500

    code test

--- module-1.jsx
+++ module-1.jsx
@@ -1,45 +1,43 @@
-// write code here and click the execute button
-
 class Button extends React.Component {
- 
- handleClick = () => {
- 	this.props.onClickFunction(this.props.incrementValue);
- }
-  
-	render() {
-  	return (
-      <button onClick={this.handleClick}>
-        +{this.props.incrementValue}
-      </button>
-    );
-  }
+
+    handleClick = () => {
+        this.props.onClickFunction(this.props.incrementValue);
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                +{this.props.incrementValue}
+            </button>
+        );
+    }
 }
 
 const Result = (props) => {
-	return (
-  	<div>{props.counter}</div>
-  );
+    return (
+        <div>{props.counter}</div>
+    );
 };
 
 class App extends React.Component {
-	state = {counter:0};
-  
-  incrementCounter = (incrementValue) => {
-    	this.setState((prevState) => ({
-    	counter: prevState.counter + incrementValue
-    }));
-  }
-  
-	render(){
-  	return (
-    	<div>
-        <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+    state = { counter: 0 };
+
+    incrementCounter = (incrementValue) => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + incrementValue
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <Button incrementValue={1} onClickFunction={this.incrementCounter} />
                 <Button incrementValue={2} onClickFunction={this.incrementCounter} />
-                                <Button incrementValue={10} onClickFunction={this.incrementCounter} />
-        <Result counter={this.state.counter} />
-      </div>
-    )
-  }
+                <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+                <Result counter={this.state.counter} />
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, mountNode);
